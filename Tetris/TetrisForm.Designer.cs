1cb6833c15c37276efaa6a3b996aabeac8a5e817
PhillyAI <phillyai314@gmail.com>
2016-04-29 09:02:36 +0900

    초기 커밋

--- Tetris/TetrisForm.Designer.cs
+++ Tetris/TetrisForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tetris
 {
-    partial class Form1
+    partial class TetrisForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TetrisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 326);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "TetrisForm";
+            this.Text = "Tetris";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
